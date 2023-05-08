b37c55bd5a0c30983c70cbffc2746ec87445233b
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-05-08 18:36:30 -0300

    first commit

---
+++ semana-1/clase1.js
@@ -0,0 +1,19 @@
+/* -------------------------------------------------------------------------- */
+/*                                  FUNCION 1                                 */
+/* -------------------------------------------------------------------------- */
+function iniciarJuego() {
+
+
+}
+
+// creamos una variable a nivel global para guardar el nombre del jugador que nos devuelve la función
+const nombreJugador = iniciarJuego();
+
+/* -------------------------------------------------------------------------- */
+/*                          CONSIGNA MESA DE TRABAJO                          */
+/* -------------------------------------------------------------------------- */
+// 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
+// 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
+// 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
+
+
