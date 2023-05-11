832eee493816362cee4338e65c8f344a91cd145c
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-05-11 00:11:56 -0300

    Clase2

--- semana-1/clase1.js
+++ semana-1/clase1.js
@@ -1,11 +1,45 @@
 /* -------------------------------------------------------------------------- */
 /*                                  FUNCION 1                                 */
 /* -------------------------------------------------------------------------- */
+// function iniciarJuego() {
+//     // Saludo al usuario
+//     alert("Bienvenido al popular juego de piedra papel o tijera de Frontend 2.");
+//     // Guardar en una variable el nombre ingresado. 
+//     const nombre = prompt("Ingrese su nombre por favor: ");
+
+//     // alert("Gracias por jugar " + nombre + ". ¡Mucha suerte!");
+//     alert(`Gracias por jugar ${nombre}. ¡Mucha suerte!`);
+
+//     console.log("---------------------");
+//     console.log("El nombre del jugador es: ");
+//     console.log(nombre);
+//     console.log("---------------------");
+    
+//     return nombre
+// }
+
+// // creamos una variable a nivel global para guardar el nombre del jugador que nos devuelve la función
+// const nombreJugador = iniciarJuego();
+
+/* -------------------------------------------------------------------------- */
+/*                          CONSIGNA MESA DE TRABAJO                          */
+/* -------------------------------------------------------------------------- */
+// 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
+// 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
+// 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
+
+
 function iniciarJuego() {
+    let nombre = "";
     // Saludo al usuario
     alert("Bienvenido al popular juego de piedra papel o tijera de Frontend 2.");
     // Guardar en una variable el nombre ingresado. 
-    const nombre = prompt("Ingrese su nombre por favor: ");
+
+    do {
+        nombre = prompt("Ingrese su nombre por favor: ");        
+    } while (!isNaN(nombre) && nombre.length >= 3);
+
+    nombre = nombre.toUpperCase()
 
     // alert("Gracias por jugar " + nombre + ". ¡Mucha suerte!");
     alert(`Gracias por jugar ${nombre}. ¡Mucha suerte!`);
@@ -20,12 +54,3 @@ function iniciarJuego() {
 
 // creamos una variable a nivel global para guardar el nombre del jugador que nos devuelve la función
 const nombreJugador = iniciarJuego();
-
-/* -------------------------------------------------------------------------- */
-/*                          CONSIGNA MESA DE TRABAJO                          */
-/* -------------------------------------------------------------------------- */
-// 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
-// 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
-// 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
-
-
--- semana-1/clase2.js
+++ semana-1/clase2.js
@@ -5,14 +5,35 @@
 // Hasta que el usuario ingrese un dato válido le seguimos pidiendo que elija.
 
 function pedirJugada() {
+    // empezar con la varialbe eleccion por defecto en 0
+    let eleccion = 0
    
+    do {
+        eleccion = parseInt(prompt("Ingrese para jugar: 1(piedra), 2(papel), 3(tijera)"))
+    } while (isNaN(eleccion) || eleccion < 1 || eleccion > 3);
+    console.log("---------------------");
+    console.log("La eleccion del jugador es: " );
+    console.log(eleccion);
+    console.log("---------------------");
+    
+    return eleccion
 }
+
 
 /* -------------------------------------------------------------------------- */
 /*                                  FUNCION 3                                 */
 /* -------------------------------------------------------------------------- */
 function jugadaRandom() {
-   
+    let numero = parseInt(Math.random() * (3 - 1) + 1)
+   /// random me da un valor entre 0 y 1 -> [0 , 1) 
+   /// Math.random() * (max - min) + min;
+    console.log("---------------------");
+    console.log("La eleccion del Computador  es: " );
+    console.log(numero);
+    console.log("---------------------");
+    
+    // finalmente retorno el valor de la eleccion aleatoria
+    return numero
 }
 
 
@@ -24,6 +45,29 @@ function jugadaRandom() {
 // Comparamos la eleccion de cada uno para saber quien pierde y quien gana.
 
 function compararJugadas() {
+    const RESULTADOS_POSIBLES = ['¡Genial, ganaste!', 'Esto fue un empate.', 'Una lástima, perdiste.'];
+
+    const eleccionJugador =  pedirJugada()
+    const eleccionComputadora = jugadaRandom()
+
+    // por defecto el jugador gana
+    let resultaRonda = RESULTADOS_POSIBLES[0]
+
+    // Revisar en caso de que empate
+    if (eleccionJugador == eleccionComputadora) {
+        resultaRonda = RESULTADOS_POSIBLES[1]
+    } else if (
+        (eleccionJugador === 1 && eleccionComputadora == 2) || (eleccionJugador === 2 && eleccionComputadora === 3) || (eleccionJugador === 3 && eleccionComputadora === 1)
+    ) {
+        resultaRonda = RESULTADOS_POSIBLES[2]       
+    }
+
+    console.log("---------------------");
+    console.log("El resultado es: " );
+    console.log(resultaRonda);
+    console.log("---------------------");
+    
+    return resultaRonda
    
 }
 
