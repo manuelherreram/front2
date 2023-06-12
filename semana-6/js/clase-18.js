4688b0cc5d06980ec214b264fbb1f45b3521f338
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-06-12 19:45:20 -0300

    Semana 6

---
+++ semana-6/js/clase-18.js
@@ -0,0 +1,39 @@
+/* -------------------------------------------------------------------------- */
+/*                [1] FUNCION: esperamos la carga de la ventana               */
+/* -------------------------------------------------------------------------- */
+
+
+/* -------------------------------------------------------------------------- */
+/*                 [2] FUNCION: capturamos los datos del form                 */
+/* -------------------------------------------------------------------------- */
+function capturarDatos() {
+
+}
+
+/* -------------------------------------------------------------------------- */
+/*                    [3] FUNCION: enviar(postear) a la API                   */
+/* -------------------------------------------------------------------------- */
+// Nos basamos en la documentacion de la API:
+// https://jsonplaceholder.typicode.com/guide/
+
+function postearComentario() {
+    // 👇 usamos nuestra funcion para capturar los datos y guardarlos como objeto
+
+}
+
+/* -------------------------------------------------------------------------- */
+/*                      [4] FUNCION: renderizar respuesta                     */
+/* -------------------------------------------------------------------------- */
+
+function renderizarRespuesta(datos) {
+
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                               MESA DE TRABAJO                              */
+/* -------------------------------------------------------------------------- */
+// En este caso vamos a trabajar la conexion con APIS,
+// en el siguiente DOC vamos a poder ver nuestra tarea
+// 👇
+// https://docs.google.com/document/d/1ZiCPf7IICvtp6rwfxoq5Wh5dJUROKqNw/preview
